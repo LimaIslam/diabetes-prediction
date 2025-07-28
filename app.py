c73122e114b02efe4987c874cbf5da1732acbbac
llima <43951243+LimaIslam@users.noreply.github.com>
2025-07-29 01:40:24 +0600

    Update app.py

--- app.py
+++ app.py
@@ -113,6 +113,6 @@ if st.button("🔍 Predict Now"):
     shap_values = explainer(pd.DataFrame(input_rounded, columns=columns))
 
     # Show SHAP force plot
-    shap.plots.waterfall(shap_values[0], show=False)
+    shap.plots.bar(shap_values[0], show=False)
     st.pyplot(plt.gcf())
     st.caption("Each bar shows how much a feature affected your result. 🔴Red means it increased the chance of diabetes, 🔵Blue means it reduced it.")
